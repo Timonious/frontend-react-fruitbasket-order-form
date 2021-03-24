8b7b1627ae8ee4f4b2e4ac9bbde72090ef123446
Your Name <timon_a@hotmail.com>
2021-03-24 21:02:22 +0100

    opdracht 'afgemaakt'

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import Counter from './Counter'
 import './App.css';
-import aardbei from './assets/aardbei.png'
-import banaan from './assets/banaan.jpg'
+import Form from "./Form";
+
 function App() {
     return (
         <>
             <h1>Fruitmand bezorgservice</h1>
-            <Counter fruitName="Aardbei" image={aardbei}/>
-            <Counter fruitName="Banaan" image={banaan}/>
+            <div className='products-wrapper'>
+                <Counter fruitName="Aardbei"/>
+                <Counter fruitName="Banaan"/>
+                <Counter fruitName="Appel"/>
+                <Counter fruitName="Kiwi"/></div>
+
+            <h2>Bestelformulier</h2><Form/>
         </>
     );
 }
